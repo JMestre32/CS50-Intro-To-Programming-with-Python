621394bf33d6cb8bec88d3ccba5e88cefe3b93f7
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-08-29 13:23:46 -0700

    Add more comments

--- Functions-and-Variables/hello.py
+++ Functions-and-Variables/hello.py
@@ -1,4 +1,6 @@
 #Notice the '=' sign. That is the assignment operator.
+#Also notice that there is a ' ' space after the ?, this is for formatting reasons.
+#We don't want the user's input to be typed right next to the ? like so "?Jacob"
 name = input("What's your name? ")  
 #The '+' sign concatenates the string and user input. 
 print("hello, " + name) 
