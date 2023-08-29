8b413899c912871e2562b916c080e42209c0a303
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-08-29 13:28:25 -0700

    Show another way of concatenation with ','

--- Functions-and-Variables/hello.py
+++ Functions-and-Variables/hello.py
@@ -4,3 +4,8 @@
 name = input("What's your name? ")  
 #The '+' sign concatenates the string and user input. 
 print("hello, " + name) 
+
+#You can also use a ',' to print the same thing. Like so:
+print("hello,", name)
+#In this case, we don't need to add an extra space in front of the first string "hello,". 
+#The comma does it for us. 
