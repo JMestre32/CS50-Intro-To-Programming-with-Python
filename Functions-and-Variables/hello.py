c962ade652398b597b4f270081dc521f2531c077
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-08-29 13:12:14 -0700

    Reformat hello.py

--- Functions-and-Variables/hello.py
+++ Functions-and-Variables/hello.py
@@ -1,2 +1,4 @@
-name = input("What's your name? ") #Notice the '=' sign. That is the assignment operator. 
-print("hello, " + name) #The '+' sign concatenates the string and user input. 
+#Notice the '=' sign. That is the assignment operator.
+name = input("What's your name? ")  
+#The '+' sign concatenates the string and user input. 
+print("hello, " + name) 
