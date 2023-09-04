7a2f075371498c3c51143c31f353bfad2bfb9589
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-04 09:04:26 -0700

    Finish up on f strings and round functions

--- Functions-and-Variables/calculator.py
+++ Functions-and-Variables/calculator.py
@@ -8,10 +8,14 @@ x = float(input("What's x? "))
 y = float(input("What's y? "))
 
 
-z = round(x + y, 0)
+#z = round(x + y, 0)
+
+z = round(x / y, 2) #this line has the same solution as line 21
 
 #There is a way to include commas in an integer using
 #the format string (f)
 
-print(f"{z:,}") 
+#print(f"{z:,}") 
 #The colon : and , output integers like 1,000
+
+print(f"{z:.2f}")
