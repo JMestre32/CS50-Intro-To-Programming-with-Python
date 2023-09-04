8acbc3a2d863f19ae3ff6bd705a821f2eefa01e5
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-04 12:07:57 -0700

    Change cat.py logic

--- Loops/cat.py
+++ Loops/cat.py
@@ -1,9 +1,9 @@
 #How can we print a string multiple times using loops? 
 
-i = 3
-while i != 0:
+i = 0
+while i <= 3:
     print("meow")
-    i -= 1
+    i += 1
 
 #Sidenote, if you accidentally make a loop that goes on infinitely
 # Ctrl + C terminates the program. 
