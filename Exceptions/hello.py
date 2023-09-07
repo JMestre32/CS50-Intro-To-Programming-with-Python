a99b5986bea31e41340b59769df1213330d246e3
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-07 10:58:28 -0700

    Fix error

---
+++ Exceptions/hello.py
@@ -0,0 +1,10 @@
+print("hello, world")
+      
+      #To fix the error above, add a quotation at the end of the string
+#In the video, the synax error is "unterminated string literal"
+
+#My syntax error says "EOL while scanning string literal" which basically
+#means the same thing, the string was not closed and the program reached
+#it's end. 
+
+#A literal is something YOU typed
