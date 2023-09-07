0b8fd441b531226ba4c44d5bfa58deefb9633324
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-07 12:29:55 -0700

    Make program more dynamic/reusable

--- Exceptions/number-modified.py
+++ Exceptions/number-modified.py
@@ -1,16 +1,19 @@
 def main():
-    x = get_int()
+    x = get_int("What's x? ")
     print(f"x is {x}")
 
-def get_int():
+def get_int(prompt):
     while True: 
         try:
-            x = int(input("What's x? "))
-            return x
+            return int(input(prompt))
         except ValueError:
             #We can pass, so we still catch the error, but we just keep 
             #prompting the user the same thing "What's x? "
             pass
-        
+
+
+#The change in prompting the user in main as opposed to in the get_int 
+#function just makes our code more dynamic and reusable. It is still 
+#functionally the same. 
 main()
 
