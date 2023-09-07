6c335126858ab88093cb946ac3ef76c806b5dfcc
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-07 12:22:16 -0700

    More optmizing

--- Exceptions/number.py
+++ Exceptions/number.py
@@ -50,16 +50,53 @@
 #There is an even better way of writing this. We don't want the program to 
 #exit if the user gives the wrong input. Instead, we want to use loops to keep
 #the program running. 
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-while True: 
-    try: 
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
-print(f"x is {x}")
+
+def get_int():
+    while True: 
+        try: 
+            #We can do this too:
+            # return int(input("What's x "))
+            #I don't like this though ^, it's just shorter
+            x = int(input("What's x? "))
+            return x
+        except ValueError:
+            print("x is not an integer")
+        #We also don't technically need the else, we can just return using
+        #the try statement since we throw an exception BEFORE returning, 
+        #it's safe to return in the try statement.
+        # else:
+        #     return x 
+        #We don't have to break AND return, we can just return
+
+main()
 
 #What this code does is validate that the user's input is actually
 #an integer. Otherwise, we stay in the loop until the user gives us what we want.
 #Once we get an integer from the user, we break out of the loop and print x.  
+#We have since turned this whole process into one function called get_int().
+#So, in our main function we have abstracted the entire thing to just 
+#2 lines of code.
+
+
+
+
+
+
+
+
+
+
+
+#This is also perfectly fine:
+
+# while True: 
+#     try: 
+#         x = int(input("What's x? "))
+#         break
+#     except ValueError:
+#         print("x is not an integer")
+# print(f"x is {x}")
