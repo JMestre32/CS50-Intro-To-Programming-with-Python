7d85ff57954f9969b26af798b481c84c4debf0c4
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-15 16:52:09 -0700

    Short __name__ desc

--- 05-Libraries/sayings.py
+++ 05-Libraries/sayings.py
@@ -1,3 +1,6 @@
+def main():
+    hello("world")
+    goodbye("world")
 def hello(name):
     print(f"hello, {name}")
 
@@ -10,4 +13,23 @@ def goodbye(name):
 
 #See say-modified.py
 
-#Be sure NOT to include main() call when making your own library
+#Be sure NOT to include a lone main() call when making your own library
+#The entire sayings.py program will run if you do!
+#Instead conditionally call main:
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+#__name__ is a special variable in python whose value is set 
+#by python to be "main" when you run this file from the command line
+#using the command: python sayings.py
+
+#__name__ is not going to be set to "main" instead it will be set to 
+#something else, technically the name of the module when you, instead, 
+#import the module like in say-modified.py
+
+#When you are IMPORTING a file, like in say-modified.py, and not 
+#running saying.py directly in the command line, main() will not get called. 
