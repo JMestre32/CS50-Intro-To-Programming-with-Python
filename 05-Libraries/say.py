50a10401647bf041d6b937914a372602811e230f
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-15 14:08:50 -0700

    ASCII ART YEAH

---
+++ 05-Libraries/say.py
@@ -0,0 +1,10 @@
+import cowsay 
+import sys
+
+# if len(sys.argv) ==2:
+#     cowsay.cow("hello, " + sys.argv[1])
+
+    #ASCII ART WOO
+
+if len(sys.argv) ==2:
+    cowsay.milk("hey, im a dragon. you are lil bro, " + sys.argv[1])
