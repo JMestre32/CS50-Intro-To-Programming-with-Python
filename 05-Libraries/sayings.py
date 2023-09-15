a1f6ebb39d9c1d047b908b2240ba844c498fa26e
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-15 16:41:51 -0700

    Make hello and good funcs

---
+++ 05-Libraries/sayings.py
@@ -0,0 +1,11 @@
+def hello(name):
+    print(f"hello, {name}")
+
+def goodbye(name):
+    print(f"goodbye, {name}")
+
+
+#How might I go about compiling functions such as these ones and
+#turning them into a library?
+
+#See say-modified.py
