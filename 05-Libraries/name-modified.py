516d5327090f71df707e1cf0eb9fb81fa69f6927
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-09-15 13:41:25 -0700

    Show how to use slices

--- 05-Libraries/name-modified.py
+++ 05-Libraries/name-modified.py
@@ -16,10 +16,23 @@ import sys
 #The format above, however, introduces a NameError/IndexError. 
 
 #The format below exits prematurely if the program cannot proceed. 
-if len(sys.argv) <2:
-    sys.exit("Too few arguments")
-elif len(sys.argv):
-    sys.exit("Too many argument")
+# if len(sys.argv) <2:
+#     sys.exit("Too few arguments")
+# elif len(sys.argv) >2:
+#     sys.exit("Too many argument")
 
 #Now it is safe to assume there is an item in sys.argv[1] and only there
-print("hello, my name is", sys.argv[1])
+# print("hello, my name is", sys.argv[1])
+
+#We now want multiple values at the prompt,
+
+if len(sys.argv) <2:
+    sys.exit("Too few arguments")
+
+#To do so, we loop through sys.argv and print the arguments given
+#to the program
+
+#Remember that argv is a list of things the user gives us, so we
+#we can treat it as such.
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
